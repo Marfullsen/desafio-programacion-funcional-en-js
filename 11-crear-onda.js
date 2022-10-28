2777f24aa03d77e8e91d2cab47183a6258e07afd
Francisco Marfull <marfullsen@gmail.com>
2022-10-28 20:38:15 -0300

    ADD: ejercicio 11 entonces.

---
+++ 11-crear-onda.js
@@ -0,0 +1,30 @@
+const boton_onda = document.getElementById('boton_onda');
+
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function crearOndaHTML() {
+    const onda = document.createElement("span");
+    const diametro = Math.max(document.documentElement.clientWidth, document.documentElement.clientHeight);
+    const radio = diametro / 2;
+    onda.style.width = onda.style.height = `${diametro}px`;
+    onda.style.left = `${random(document.documentElement.clientWidth) - radio}px`;
+    onda.style.top = `${random(document.documentElement.clientHeight) - radio}px`;
+    onda.classList.add('onda');
+    return onda;
+}
+
+//const { fromEvent } = rxjs;
+const { /* take, */ mergeMap, finalize } = rxjs.operators;
+
+fromEvent(boton_onda, 'click').pipe(mergeMap(()=> crearOnda()))
+.subscribe(console.log);
+
+function crearOnda(){
+    const onda = crearOndaHTML();
+    document.body.appendChild(onda);
+    return fromEvent(onda, 'animationend').pipe(
+        take(1),
+        finalize(()=> document.body.removeChild(onda)));
+}
