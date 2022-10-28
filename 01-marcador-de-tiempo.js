c0c8f39587960d7dd10e9b24e12ecf9ae20453f2
Francisco Marfull <marfullsen@gmail.com>
2022-10-27 23:13:27 -0300

    Init: Base y primer ejercicio.

---
+++ 01-marcador-de-tiempo.js
@@ -0,0 +1,13 @@
+// Marcador de tiempo.
+
+const resultado = document.getElementById('resultado');
+
+const { timer } = rxjs;
+const { map } = rxjs.operators;
+
+timer(1000, 1000).pipe(map(()=>{
+  const ahora = new Date();
+  return ahora.toLocaleString();
+})).subscribe(valor => {
+  resultado.innerText = valor;
+})
